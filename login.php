de2aae018ae754f01a2490f22498231aa7b6db88
milirin <milirin07@gmail.com>
2021-06-26 15:18:52 +0500

    Revert "Added password property to Login class. Added construct to init email and password."
    This reverts commit 43dc614ac6cc1cb8eda1a065a533f5089b9a7d14.

--- login.php
+++ login.php
@@ -2,11 +2,4 @@
 
 class Login {
     public string $email;
-    public string $password;
-
-    public function __construct(string $email, string $password)
-    {
-        $this->email = $email;
-        $this->password = $password;
-    }
 }
