d5eb4d6d3e8c9954b1cda0c6068750e385a716e5
milirin <milirin07@gmail.com>
2021-06-28 20:33:50 +0500

    Fix user-profile

--- UserProfile.php
+++ UserProfile.php
@@ -4,4 +4,11 @@ class UserProfile{
   public string $name;
   public int $age;
   public string $avatar;
+
+  public function __construct($name, $age, $avatar)
+  {
+    $this->name = $name;
+    $this->age = $age;
+    $this->avatar = $avatar;
+  }
 }
